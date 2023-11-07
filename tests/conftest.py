ce38bf965a387c002cc26aa0b3d47be9f12a11a5
Alison Carlos Santos <alison-carlos@hotmail.com>
2023-11-07 08:34:12 -0300

    implementação do refresh token e respectivos testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import factory
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
@@ -8,6 +9,16 @@ from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import Base, User
 from fast_zero.security import get_password_hash
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+
+    id = factory.Sequence(lambda n: n)
+    username = factory.LazyAttribute(lambda obj: f'test{obj.id}')
+    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
+    password = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
 
 
 @pytest.fixture
@@ -52,6 +63,19 @@ def user(session):
 
     user.clean_password = 'testtest'
 
+    return user
+
+
+@pytest.fixture
+def other_user(session):
+    password = 'testtest'
+    user = UserFactory(password=get_password_hash(password))
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    user.clean_password = 'testtest'
     return user
 
 
---
+++ tests/test_users.py
@@ -0,0 +1,96 @@
+from fast_zero.schemas import UserPublic
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'carlos',
+            'email': 'carlos@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'carlos',
+        'email': 'carlos@example.com',
+        'id': 1,
+    }
+
+
+def test_create_user_exception_already_registered(client):
+
+    test_create_user(client)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'carlos',
+            'email': 'carlos@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Email already registered'}
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == 200
+    assert response.json() == {'users': []}
+
+
+def test_update_user(client, user, token):
+    response = client.put(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == 200
+    assert response.json() == {'detail': 'User deleted'}
+
+
+def test_delete_user_wrong_user(client, other_user, token):
+    response = client.delete(
+        f'/users/{other_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Not enough permissions'}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user_with_wrong_user(client, other_user, token):
+    response = client.put(
+        f'/users/{other_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Not enough permissions'}
