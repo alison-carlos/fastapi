c62aa34d0d6c6f23ad30fce33da01e1987b21536
Alison Carlos Santos <alison-carlos@hotmail.com>
2023-11-19 11:10:49 -0300

    implementação dos metodos de inclusao, delete, atualizacao para o endpoint todo

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from fast_zero.routers import auth, users
+from fast_zero.routers import auth, todos, users
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todos.router)
 
 
 @app.get('/')
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,10 @@
 from sqlalchemy import select
+from sqlalchemy.orm import Session
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
-def test_create_user(session):
+def test_create_user_without_todos(session):
     new_user = User(
         username='alison', password='secret123', email='teste@teste.com.br'
     )
@@ -13,3 +14,20 @@ def test_create_user(session):
     user = session.scalar(select(User).where(User.username == 'alison'))
 
     assert user.username == 'alison'
+
+
+def test_create_todo(session: Session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Desc',
+        state='draft',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
